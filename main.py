72fd300a132c35ff8db01e11ca573877bdd222d6
kinako1415 <Owata09@outlook.jp>
2025-10-16 13:40:18 +0900

    環境構築

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-sdf
+from diaries.DiarySample import DiarySample
+
+# ↓のリストには、メンバーの各日記が格納されます。
+diaries = [DiarySample(), ] 
+
+for d in diaries:
+    print("---------------------------------")
+    print(d.get_date())
+    print(d.get_summary())
+    print(d.get_author())
+    print()
